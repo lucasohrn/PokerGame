842178143515b5fe97621698b546552814ae7123
unknown <sixtenglarsson@gmail.com>
2020-11-10 01:22:57 +0100

    added Hand in playerscript

--- PokerLib/Player.cs
+++ PokerLib/Player.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Linq;
 
 namespace Poker
 {
     class Player : IPlayer
     {
+        public string Name => name;
         private string name;
+
+        public int Wins => wins;
         private int wins;
 
         public Player(string playerName, int playerWins)
@@ -13,17 +17,33 @@ namespace Poker
             wins = playerWins;
         }
 
-        private ICard[] hand;
-    
-        private HandType handType;
-        public HandType HandType => handType;
-        
-        public string Name => name;
-
         public ICard[] Hand => hand;
+        private ICard[] hand;
 
+        public HandType HandType => handType;
+        private HandType handType;
 
-        public int Wins => wins;
+        void SortCards() // inte testad ska kunna sortera men stor möjlighet att den sorterar fel, jag kan inte linq
+        {
+            var sorted = hand.GroupBy(x => x.Suite).Select(x => new
+            {
+                Cards = x.OrderByDescending(c => c.Rank),
+                Count = x.Count(),
+            }
+            ).OrderByDescending(x => x.Count).SelectMany(x => x.Cards);
+        }
+
+        void RecieveCard(ICard card)
+        {
+            for (int i = 0; i < hand.Length; i++)
+            {
+                if (hand[i] == null)
+                {
+                    hand[i] = card;
+                    return;
+                }
+            }
+        }
 
         public ICard[] Discard { set => throw new NotImplementedException(); }
     }
