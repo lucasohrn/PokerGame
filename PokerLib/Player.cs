fbd22d20110389f262ac881fb85f1c99c7bbd00d
unknown <sixtenglarsson@gmail.com>
2020-11-11 14:55:28 +0100

    GetHandType now able to get handtype (proably)

--- PokerLib/Player.cs
+++ PokerLib/Player.cs
@@ -36,11 +36,6 @@ namespace Poker
             }
         }
 
-        void GetHandType()
-        {
-            throw new NotImplementedException();
-        }
-
         void SortCards() // inte testad ska kunna sortera men stor möjlighet att den sorterar fel, jag kan inte linq
         {
             var sorted = hand.GroupBy(x => x.Suite).Select(x => new
@@ -51,10 +46,106 @@ namespace Poker
             ).OrderByDescending(x => x.Count).SelectMany(x => x.Cards);
         }
 
-        
+
         List<ICard[]> graveYardCards = new List<ICard[]>();
 
         public ICard[] Discard { set => graveYardCards.Add(value); }
 
+        HandType GetHandType()
+        {
+            bool straight = IsStraight(hand);
+            bool allSameSuit = IsAllSameSuit(hand);
+
+            if (straight && allSameSuit)
+            {
+                if ((int)hand[0].Rank == 10)
+                {
+                    return HandType.RoyalStraightFlush;
+                }
+                else
+                    return HandType.StraightFlush;
+            }
+
+            List<int> sameCardSet1, sameCardSet2;
+            FindSetsOfCardsWithSameValue(hand, out sameCardSet1, out sameCardSet2);
+
+            if (sameCardSet1.Count == 4)
+                return HandType.FourOfAKind;
+
+            if (sameCardSet1.Count + sameCardSet2.Count == 5)
+                return HandType.FullHouse;
+
+            if (allSameSuit)
+                return HandType.Flush;
+
+            if(straight)
+                return HandType.Straight;
+
+            if (sameCardSet1.Count == 3)
+                return HandType.ThreeOfAKind;
+
+            if (sameCardSet1.Count + sameCardSet2.Count == 4)
+                return HandType.TwoPairs;
+
+            if (sameCardSet1.Count == 2)
+                return HandType.Pair;
+
+            return HandType.HighCard;
+        }
+
+        bool IsStraight(ICard[] hand)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (hand[i].Rank == hand[i + 1].Rank)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool IsAllSameSuit(ICard[] hand)
+        {
+            Suite suite = hand[0].Suite;
+            for (int i = 1; i < hand.Length; i++)
+            {
+                if (suite != hand[i].Suite)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        void FindSetsOfCardsWithSameValue(ICard[] pokerHand, out List<int> sameValueSet1, out List<int> sameValueSet2)
+        {
+            //Find sets of cards with the same value.
+            int index = 0;
+            sameValueSet1 = FindSetsOfCardsWithSameValue_Helper(pokerHand, ref index);
+            sameValueSet2 = FindSetsOfCardsWithSameValue_Helper(pokerHand, ref index);
+        }
+
+        List<int> FindSetsOfCardsWithSameValue_Helper(ICard[] pokerHand_ArrangedCorrectly, ref int index)
+        {
+            List<int> sameCardSet = new List<int>();
+            for (; index < 4; index++)
+            {
+                int currentCard_intValue = (int)pokerHand_ArrangedCorrectly[index].Rank;
+                int nextCard_intValue = (int)pokerHand_ArrangedCorrectly[index + 1].Rank;
+                if (currentCard_intValue == nextCard_intValue)
+                {
+                    if (sameCardSet.Count == 0)
+                        sameCardSet.Add(currentCard_intValue);
+                    sameCardSet.Add(currentCard_intValue);
+                }
+                else if (sameCardSet.Count > 0)
+                {
+                    index++;
+                    break;
+                }
+            }
+            return sameCardSet;
+        }
     }
 }
