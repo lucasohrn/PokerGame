a1a4ce0f550e4febbc8bf6a953f22cae6d0b7752
Lucas Öhrn <lucas.ohrn@codeitab.se>
2020-12-09 11:36:40 +0100

    Change dealer

--- PokerLib.UnitTest/DealerTest.cs
+++ PokerLib.UnitTest/DealerTest.cs
@@ -4,16 +4,27 @@ using static Poker.Suite;
 using static Poker.Rank;
 using Poker;
 using System.Linq;
-using System.Test.RegularExpressions;
+using System.Text.RegularExpressions;
 
 namespace PokerLib.UnitTest
 {
     [TestFixture]
     public class DealerTest
     {
+        class MockDeck : Deck
+        {
+            public MockDeck(int numberOfPlayers, string[] hands)
+            {
+                
+            }
+
+
+        }
+
         [Test]
         public void DealerCanDeal()
         {
+            /*
             Dealer dealer = new Dealer(new Deck());
 
             Player[] players = new Player[]
@@ -25,8 +36,8 @@ namespace PokerLib.UnitTest
             dealer.Deal(players);
 
             Assert.AreEqual(players[0].Hand.Count(), 5, players[0].Name);
-            Assert.AreEqual(players[1].Hand.Count(), 5, players[1].Name);
-
+            Assert.AreEqual(players[1].);
+            */
 
 
         }
